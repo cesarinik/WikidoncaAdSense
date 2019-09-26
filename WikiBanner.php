89a5f4d4236fc88632270775fc9f5527d60b90ef
cesarinik <55831620+cesarinik@users.noreply.github.com>
2019-09-26 10:44:39 +0200

    Update WikiBanner.php

--- WikiBanner.php
+++ WikiBanner.php
@@ -1,14 +1,4 @@
 <?php
-$wgExtensionCredits['other'][] = array(
-        'path' => __FILE__,
-        'name' => "WikiBanner",
-        'description' => "Allows top and bottom banners to be added to the wiki as configured in the LocalSettings.php file.",
-        'version' => '1.2 beta',
-        'author' => "maelstr0m e [https://www.mediawiki.org/wiki/User:Cesarinik Riccardo Cesarini]",
-        'url' => "http://www.mediawiki.org/wiki/Extension:WikiBanner",
-	'license-name' => 'MIT'
-);
-
 $wgHooks['BeforePageDisplay'][] = 'Banner::InsertAds';
 if (!class_exists('Banner')) {
 if (!function_exists('InsertAds'))   {
